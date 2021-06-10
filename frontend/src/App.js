0bee913f8d708696a35fd721f72723a8d960411e
Poojitha Manda <poojithamanda9581@gmail.com>
2021-06-11 00:53:57 +0530

    'react_setup'

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <h1>Welcome to NutsBite</h1>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
